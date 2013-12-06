8924bcfa8b090ca78261f31a830d2ba80dbdb4bb
Cosmin <cmolea@UnitedMotionIdeas.com>
2013-12-06 21:45:20 +0100

    Add User model and controller on Sails backend.

---
+++ api/models/User.js
@@ -0,0 +1,28 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ *
+ */
+
+module.exports = {
+
+  attributes: {
+
+    attributes: {
+
+      name: {
+          type: 'string',
+          required: true
+      },
+
+      email: {
+          type: 'string'
+      }
+
+    }
+    
+  }
+
+};
