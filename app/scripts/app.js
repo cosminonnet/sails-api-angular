eaff8c5958f23103e3595de2c89b441ef26c1b9d
Cosmin <cmolea@UnitedMotionIdeas.com>
2013-12-12 16:27:20 +0100

    Add the about, features, customers and pricing pages.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,18 @@ angular.module('sailsApiAngularApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/features', {
+        templateUrl: 'views/features.html'
+      })
+      .when('/customers', {
+        templateUrl: 'views/customers.html'
+      })
+      .when('/pricing', {
+        templateUrl: 'views/pricing.html'
+      })
       .otherwise({
         redirectTo: '/'
       });
